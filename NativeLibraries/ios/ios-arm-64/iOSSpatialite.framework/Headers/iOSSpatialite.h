c241ec871d55f7566c1ada5d6fe6982076f5fccb
Mihai Muntean <mihai.muntean@outlook.com>
2020-04-03 12:25:34 +0100

    added framework with arm-64 support

---
+++ NativeLibraries/ios/ios-arm-64/iOSSpatialite.framework/Headers/iOSSpatialite.h
@@ -0,0 +1,19 @@
+//
+//  iOSSpatialite.h
+//  iOSSpatialite
+//
+//  Created by Mihai Muntean on 03/04/2020.
+//  Copyright © 2020 Mihai Muntean. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for iOSSpatialite.
+FOUNDATION_EXPORT double iOSSpatialiteVersionNumber;
+
+//! Project version string for iOSSpatialite.
+FOUNDATION_EXPORT const unsigned char iOSSpatialiteVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iOSSpatialite/PublicHeader.h>
+
+
